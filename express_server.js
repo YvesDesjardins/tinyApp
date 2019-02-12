23ef1e805b28a22cf917c458f4c73c4727385543
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-12 14:09:45 -0800

    updated weight of header text and added handling route for urls/new

--- express_server.js
+++ express_server.js
@@ -17,6 +17,9 @@ app.get("/urls", (req, res) => {
   };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
